cfe3f971ff1a6409a3f38de35f87c9cae630c542
ris-tlp <omarkhantlp@gmail.com>
2022-08-08 04:40:46 +0300

    .env for secrets/credentials

--- wijha_api_conf/settings.py
+++ wijha_api_conf/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a9=fl#*@@z(o!qb$03hn-1mtr)-_4dr&z&zotl_^lwvmmfsq^b"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
