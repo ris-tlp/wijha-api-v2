d2e221ef1cc8c7085faa0980563d62f415aa5b97
ris-tlp <omarkhantlp@gmail.com>
2022-09-16 09:32:47 +0300

    docker images for dev and prod

---
+++ dev.sh
@@ -0,0 +1,1 @@
+docker run -ti -p 8081:8081 -v "$(pwd):/wijha-api-v2/" -v ~/.aws/:/root/.aws --rm wijha-api-v2-dev:latest bash
--- zappashell.sh
+++
@@ -1,1 +0,0 @@
-docker run -ti -p 8000:8000 -v "$(pwd):/var/task" -v ~/.aws/:/root/.aws --rm zappa-docker-image:latest
